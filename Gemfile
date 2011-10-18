e4e3e44999b063df93fe7cdcceeb51848824c473
Avrohom Katz <akatz@engineyard.com>
2011-10-18 11:31:57 -0700

    bundler

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# A sample Gemfile
+source "http://rubygems.org"
+
+ gem "rails", "2.3.3"
+ gem "ufo", :require => "flying_saucer", :git => "https://github.com/wycats/rufo.git"
+ gem "activerecord-jdbcmysql-adapter"
+ #gem "thinking-sphinx"
+
--- config/environment.rb
+++ config/environment.rb
@@ -51,4 +51,16 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.3.7')
+ module Rails
+   class GemDependency
+     def requirement
+       r = super
+       (r == Gem::Requirement.default) ? nil : r
+     end
+   end
+ end
+end
+  
 end
